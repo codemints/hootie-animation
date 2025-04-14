447620777d46bfc6c80887631175b477c880a2c3
Daniel LittleThunder <daniel.littlethunder@moxiesozo.com>
2025-04-13 20:00:50 -0600

    Finished animation of clouds and hootie, and refactored the HTML and CSS.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,67 @@
-import './style.css'
-import './clouds.css'
-import './clouds.js'
+import './main.css'
+import './animation-definitions.css'
+import './animation-content.css'
 
-let isAnimating = false
-const animateButton = document.querySelector('.animate-button')
-const animateWrapper = document.querySelector('.hootie-animation-wrapper')
+let isStarted = false
+let isPlaying = false
+let isPaused = false
+const controlsWrapper = document.querySelector('.animation-controls')
+const animationWrappers = Array.from(document.querySelectorAll('.animation-wrapper'))
+const animatedItems = Array.from(document.querySelectorAll('.animation-item-wrapper'))
+const playButton = controlsWrapper.querySelector('.play-button')
+const pauseButton = controlsWrapper.querySelector('.pause-button')
+const resetButton = controlsWrapper.querySelector('.reset-button')
 
-animateButton.addEventListener('click', () => {
-  if (!isAnimating) {
-    animateWrapper.classList.add('animation-animated')
-    animateButton.innerText = "Un-animate"
-    isAnimating = true
-  } else {
-    animateWrapper.classList.remove('animation-animated')
-    animateButton.innerText = "Animate"
-    isAnimating = false
+if (!isPlaying) {
+  pauseButton.setAttribute('disabled', 'disabled')
+  resetButton.setAttribute('disabled', 'disabled')
+}
+
+playButton.addEventListener('click', (e) => {
+  if (isPaused) {
+    isPaused = false
+    animatedItems.forEach(item => {
+      item.style.animationPlayState = 'running'
+    })
   }
+
+  isPlaying = true
+  isStarted = true
+  playButton.setAttribute('disabled', 'disabled')
+  pauseButton.removeAttribute('disabled')
+  resetButton.removeAttribute('disabled')
+
+  animationWrappers.forEach(wrapper => {
+    wrapper.classList.add('animation-animated')
+  })
+})
+
+pauseButton.addEventListener('click', (e) => {
+  if (!isStarted) e.preventDefault()
+  isPlaying = false
+  isPaused = true
+  playButton.removeAttribute('disabled')
+  pauseButton.setAttribute('disabled', 'disabled')
+
+  animatedItems.forEach(item => {
+    item.style.animationPlayState = 'paused'
+  })
+})
+
+resetButton.addEventListener('click', (e) => {
+  if (!isStarted) e.preventDefault()
+  isPlaying = false
+  isStarted = false
+  isPaused = false
+  playButton.removeAttribute('disabled')
+  pauseButton.setAttribute('disabled', 'disabled')
+  resetButton.setAttribute('disabled', 'disabled')
+
+  animatedItems.forEach(item => {
+    item.style.animationPlayState = ''
+  })
+
+  animationWrappers.forEach(wrapper => {
+    wrapper.classList.remove('animation-animated')
+  })
 })
